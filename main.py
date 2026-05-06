a64a276c157290ac6cd5bce34fdc42e355f08185
Wisdom Alawode <alawodewisdom02@gmail.com>
2026-05-06 21:30:47 +0100

    creating api endpoints for all operations

--- main.py
+++ main.py
@@ -2,3 +2,24 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+#create posts
+@app.post("/post")
+def create_post():
+    return {"message":"successful"}
+
+#view all posts
+@app.get("/posts")
+def view_posts_all():
+    return {"data":"data here"}
+
+#view post by title
+@app.get("/posts/{title}")
+def view_posts_title():
+    return {"data": "ypu"}
+
+#view posts by tag
+@app.get("/posts/{tag}")
+def view_posts_tag():
+    return {"data","taghe"}
